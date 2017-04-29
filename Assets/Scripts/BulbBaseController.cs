59a50e8c92b7e2f798b87ef9b784a5e19cddd4c1
nice-shot <shefferu@gmail.com>
2017-04-29 15:23:39 +0300

    added glow effect for object near the player

---
+++ Assets/Scripts/BulbBaseController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulbBaseController : MonoBehaviour {
+    public GameObject glowEffect;
+
+    // Use this for initialization
+    void Start() {
+    }
+	
+    // Update is called once per frame
+    void Update() {
+		
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            glowEffect.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            glowEffect.SetActive(false);
+        }
+    }
+
+//    void OnTriggerStay2D(Collider2D other) {
+//        Debug.Log("Player in me!");
+//    }
+}
