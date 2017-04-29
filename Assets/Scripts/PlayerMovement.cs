babf020f01302e0c0fd7d6bd912c620d0eb4c995
syoels <syoels@gmail.com>
2017-04-29 14:30:28 +0300

    Added light, camera follow and base player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed = 240f;
+    private Rigidbody2D body; 
+
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 velocity = body.velocity;
+        float movement = Input.GetAxis("Horizontal");
+        velocity.x = movement * Time.deltaTime * moveSpeed;
+        body.velocity = velocity;
+
+
+    }
+}
