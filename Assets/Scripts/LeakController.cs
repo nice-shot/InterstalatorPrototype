b22be33c9e9f72c2736091ebe139e96b9f3de6b5
nice-shot <shefferu@gmail.com>
2017-05-01 12:29:26 +0300

    Added text box above leak and added interaction to change it

--- Assets/Scripts/LeakController.cs
+++ Assets/Scripts/LeakController.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LeakController : Interactable {
-    public override void Interact(PlayerController player) {
-        Debug.Log("You put duck tape on me!");
+
+    public TextMesh textBox;
+
+    private bool _isSealed = false;
+    private bool isSealed {
+        get {
+            return _isSealed;
+        }
+        set {
+            _isSealed = value;
+            if (_isSealed) {
+                textBox.text = "Sealed Leak";
+                // TODO: Add code to change electricity generator here.
+            } else {
+                textBox.text = "Leak";
+            }
+        }
     }
 
+    void Start() {
+        textBox = transform.GetComponentInChildren<TextMesh>();
+    }
+
+    public override void Interact(PlayerController player) {
+        isSealed = !isSealed;
+    }
+
+    // The player needs ducktape to interact
     public override bool CanInteract(PlayerController player) {
         return player.heldItem != null && player.heldItem.type == ItemType.Tape;
     }
