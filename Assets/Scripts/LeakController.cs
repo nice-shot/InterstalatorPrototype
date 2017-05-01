aa85754fd96ff485be70ce40934743b9740b93ac
nice-shot <shefferu@gmail.com>
2017-05-01 12:19:19 +0300

    added the leak prefab and started writing its script

---
+++ Assets/Scripts/LeakController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeakController : Interactable {
+    public override void Interact(PlayerController player) {
+        Debug.Log("You put duck tape on me!");
+    }
+
+    public override bool CanInteract(PlayerController player) {
+        return player.heldItem != null && player.heldItem.type == ItemType.Tape;
+    }
+}
