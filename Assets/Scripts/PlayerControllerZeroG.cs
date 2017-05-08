f259b25d407cc2b7821a77608dc78e5a972d0200
nice-shot <shefferu@gmail.com>
2017-05-08 23:42:03 +0300

    player will now start floating when gravity changes

--- Assets/Scripts/PlayerControllerZeroG.cs
+++ Assets/Scripts/PlayerControllerZeroG.cs
@@ -3,6 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllerZeroG : PlayerController {
+    private bool isZeroG;
+
+    override protected void Start() {
+        base.Start();
+        isZeroG = body.gravityScale == 0;
+
+    }
+
+    override protected void Update() {
+        if ((body.gravityScale == 0) != isZeroG) {
+            isZeroG = !isZeroG;
+            ChangedToZeroG();
+        }
+        base.Update();
+    }
+
+    override protected void HandleMovement() {
+        if (body.gravityScale != 0) {
+            base.HandleMovement();
+            return;
+        }
+        Debug.Log("Trying to move in 0 G");
+    }
+
     override protected void DropItem() {
         ZeroGravityItem zeroGItem = (ZeroGravityItem)heldItem;
         zeroGItem.mainItem.transform.SetParent(null);
@@ -16,5 +40,27 @@ public class PlayerControllerZeroG : PlayerController {
         collider.isTrigger = false;
 
         heldItem = null;
+    }
+
+    private void ChangedToZeroG() {
+        if (!isZeroG) {
+            return;
+        }
+        // Reduce velocity to avoid flying to the other side
+        Vector2 newInitialVelocity = body.velocity;
+        newInitialVelocity.x *= 0.1f;
+        body.velocity = newInitialVelocity;
+
+        Debug.Log("Changing gravity");
+        float verticalForce = Random.Range(8f, 15f);
+        float horizontalForce = Random.Range(-4f, 4f);
+
+        Vector2 forceUpwards = new Vector2(horizontalForce, verticalForce);
+        Debug.Log("Adding upwards force: " + forceUpwards);
+        body.AddForce(forceUpwards);
+
+        float rotationForce = Random.Range(0.2f, 4f);
+        Debug.Log("Adding rotation force: " + rotationForce);
+        body.AddTorque(rotationForce);
     }
 }
