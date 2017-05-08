12686584bfb4d0cd1e5de2e21a90b6f6b1ee99a4
nice-shot <shefferu@gmail.com>
2017-05-08 22:37:10 +0300

    trying to fuse lightbulb and zerogravity agent. still have a few issues with grabbing and dropping the item

---
+++ Assets/Scripts/ZeroGravityItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeroGravityItem : Item {
+    private GameObject mainItem;
+    void Start() {
+        mainItem = transform.parent.gameObject;
+    }
+
+    public override void Interact(PlayerController player) {
+        // Place the item graphic at the center of the player
+        mainItem.transform.parent = player.transform;
+        mainItem.transform.localPosition = new Vector2(0, 0);
+        SpriteRenderer sprite = mainItem.GetComponent<SpriteRenderer>();
+        sprite.sortingLayerName = "Forground";
+
+        // Deactivate the rigidbody
+        Rigidbody2D body = mainItem.GetComponent<Rigidbody2D>();
+        body.Sleep();
+
+        // Add this item to the player logic
+        player.heldItem = this;
+    }
+}
