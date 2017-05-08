56e8bfacc33411751d3730ab2f7d94eb03d30692
nice-shot <shefferu@gmail.com>
2017-05-08 23:11:32 +0300

    added gravity control panel - now the player can change gravity at will and pick items up while it happens

---
+++ Assets/Scripts/GravitySwitchController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySwitchController : Interactable {
+
+    public Rigidbody2D[] bodysWithGravity;
+    private bool gravityOn = true;
+
+    public override void Interact(PlayerController player) {
+        gravityOn = !gravityOn;
+
+        // 1 is the basic gravity scale for all
+        float gravityScale = gravityOn ? 1f : 0f;
+
+        foreach (Rigidbody2D body in bodysWithGravity) {
+            body.gravityScale = gravityScale;
+        }
+    }
+
+    public override bool CanInteract(PlayerController player) {
+        // Can only be interactable if the player doesn't hold any items
+        return player.heldItem == null;
+    }
+}
